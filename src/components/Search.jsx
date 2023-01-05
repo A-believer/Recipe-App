97c00eba1e809beac30586adc30fd7e6a3b7a551
davedev <davidabolade29@gmail.com>
2023-01-05 16:31:16 +0100

    update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,13 +2,26 @@ import { useState } from "react"
 import { useGlobalContext } from "../context"
 
 const Search = () => {
+    const [text, setText] = useState("")
+    
+const { setSearchText } = useGlobalContext();
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (text) {setSearchText(text)
+            setText('')
+        }
+    }
 
-    const { } = useGlobalContext();
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+
+    
     return (
         <header className="search-container">
-            <form action="">
-                <input type="text" className="form-input"  placeholder="type favorite meal"/>
-                <button className="btn" type="submit">search</button>
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" className="form-input"  placeholder="type favorite meal" value={text} onChange={handleChange}/>
+                <button className="btn" type="submit">Search</button>
                 <button className="btn btn-hipster" type="button">Surprise Me!</button>
             </form>
         </header>
